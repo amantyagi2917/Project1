47682a685bd0e4fa2031d01514003b3d324d25de
Aman Tyagi <aman@gmail.com>
2026-01-03 14:25:25 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connection= async()=>{
-    await mongoose.connect('mongodb://localhost:27017/bookstore').then(()=>{
+    await mongoose.connect('mongodb+srv://amantyagi3132:aman132217tyagi@cluster0.wdyih9t.mongodb.net/bookstore').then(()=>{
 
         console.log('database connected');
         
